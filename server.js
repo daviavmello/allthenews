820444e84b689160118641a187e8401cc9bf13b4
Davi Mello <davimello1994@gmail.com>
2019-10-22 12:21:20 -0400

    Setting Up for Heroku

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ layout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect(
-    process.env.MONGODB_URI ||
-    "mongodb://user:password1@ds137498.mlab.com:37498/heroku_4168v2ff",
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/allthenews-scrapper";
 
-{
-    useMongoClient: true
-}
-);
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+require("./routes/routes.js")(app);
 
 app.listen(3000, function () {
     console.log("App running on port 3000!");
