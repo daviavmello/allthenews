fbf51c3a77e30ec42dc44a1a682ff991cfbd6b1e
Davi Mello <davimello1994@gmail.com>
2019-10-23 10:40:45 -0400

    Setting up to Heroku

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ let express = require("express");
 let mongoose = require("mongoose");
 let exphbs = require("express-handlebars");
 
+var PORT = process.env.PORT || 3000;
+
 // Initialize Express
 let app = express();
+
+app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -14,7 +18,8 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/allthenews-scrapper";
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 require("./routes/routes.js")(app);
 
 app.listen(3000, function () {
